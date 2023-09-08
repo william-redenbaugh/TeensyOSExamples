9fc6364bc5707588cd37be509f4545c57b4f0386
William Redenbaugh <wmredenbaugh@gmail.com>
2023-09-08 16:09:07 -0700

    forgot to add the test code in the code entry

--- TeensyOSTesting/src/main.cpp
+++ TeensyOSTesting/src/main.cpp
@@ -1,9 +1,13 @@
 #include <Arduino.h>
-
+#include "global_includes.h"
+#include "OS/OSThreadKernel.h"
 void setup()
 {
+    threads_init();
+    threads_init();
 }
 
 void loop()
 {
+    os_thread_delay_s(20);
 }
