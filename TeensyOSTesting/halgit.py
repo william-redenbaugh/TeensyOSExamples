57cd8b2ce488e606df367e91d06bcb97acd8c03f
William Redenbaugh <wmredenbaugh@gmail.com>
2023-09-13 17:28:19 -0700

    updating subrepos, adding checkout functionality to halgit, adding the TeensyOS to the repos.json module

--- TeensyOSTesting/halgit.py
+++ TeensyOSTesting/halgit.py
@@ -5,10 +5,12 @@ import subprocess
 def run_command(command):
     try:
         # Run the command and capture its output
-        output = subprocess.check_output(command, shell=True, stderr=subprocess.STDOUT, universal_newlines=True)
+        output = subprocess.check_output(
+            command, shell=True, stderr=subprocess.STDOUT, universal_newlines=True)
         return output
     except subprocess.CalledProcessError as e:
         print(e)
+
 
 def open_repos():
 
@@ -22,19 +24,41 @@ def open_repos():
 
     return data
 
+
 def command_repos(repos, sub_command):
     try:
         # Print the deserialized data
         for key, value in repos.items():
             print("Repo: ", key)
             command = None
-            if(value["dir"] != ""):
+            if (value["dir"] != ""):
                 command = "cd " + value["dir"] + " && " + sub_command
             else:
-                command =  sub_command
+                command = sub_command
             output = run_command(command)
             print(output)
-    
+
+    except Exception as e:
+        print("Invalid repos.json parameters")
+        print(e)
+
+def checkout_repos(repos, branch):
+    try:
+        # Print the deserialized data
+        for key, value in repos.items():
+            print("Repo: ", key)
+            command = None
+
+            if(branch == None):
+                branch = value["branch"]
+            if (value["dir"] != ""):
+                command = "cd " + value["dir"] + " && " + \
+                    "git checkout " + branch
+            else:
+                command = "git checkout " + branch
+            output = run_command(command)
+            print(output)
+
     except Exception as e:
         print("Invalid repos.json parameters")
         print(e)
@@ -45,26 +69,33 @@ def push_repos(repos):
         for key, value in repos.items():
             print("Repo: ", key)
             command = None
-            if(value["dir"] != ""):
-                command = "cd " + value["dir"] + " && " + "git push origin " + value["branch"]
+            if (value["dir"] != ""):
+                command = "cd " + value["dir"] + " && " + \
+                    "git push origin " + value["branch"]
             else:
-                command =  "git push origin " + value["branch"]
+                command = "git push origin " + value["branch"]
             output = run_command(command)
             print(output)
-    
+
     except Exception as e:
         print("Invalid repos.json parameters")
         print(e)
 
+
 def parse_args():
     parser = argparse.ArgumentParser(description="Pull or clear JSON data")
-    parser.add_argument("action", choices=["pull", "clear", "add", "commit", "push", "status"], help="Action to perform (pull or clear)")
-    parser.add_argument('-m', '--message', type=str, required=False, help='The message to display.')
-
+    parser.add_argument("action", choices=[
+                        "pull", "clear", "add", "commit", "push", "status", "pull", "checkout"], help="Action to perform (pull or clear)")
+    parser.add_argument('-m', '--message', type=str,
+                        required=False, help='Commit message')
+    
+    parser.add_argument('-b', '--branch', type=str,
+                        required=False, help='Targeted branch')
     # Parse the command-line arguments
     args = parser.parse_args()
 
     return args
+
 
 def main():
     args = parse_args()
@@ -72,19 +103,23 @@ def main():
 
     action = args.action
     message = args.message
-    if(action == "pull"):
+    branch = args.branch
+    if (action == "pull"):
         command_repos(repos, "git pull")
-    elif(action == "clear"):
+    elif (action == "clear"):
         command_repos(repos, "git checkout .")
-    elif(action == "add"):
+    elif (action == "add"):
         command_repos(repos, "git add .")
-    elif(action == "commit"):
+    elif (action == "commit"):
         command_repos(repos, "git commit -m \"" + message + "\"")
-    elif(action == "push"):
+    elif (action == "push"):
         push_repos(repos)
-    elif(action == "status"):
+    elif (action == "status"):
         command_repos(repos, "git status")
+    elif(action == "checkout"):
+        checkout_repos(repos, branch)
     else:
         print("invalid input")
+
 
 main()
